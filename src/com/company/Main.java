e7990aa2d84769398ef40bf70650a02357a1a583
Yannis Lagrosa <378940@ga.aliefisd.net>
2022-07-23 18:05:37 -0500

    Updated Files.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        boolean checking = true;
         float[] arr = {0f,1f,2f,3f,4f,5f,6f,7f};
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What's your target?");
-        float target = scanner.nextFloat();
-        System.out.println(RecursiveBinarySearch.recursiveBinarySearch(arr,target));
+        while(checking) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("What's your target?");
+            float target = scanner.nextFloat();
+            System.out.println(RecursiveBinarySearch.myBinaryRecursionSearch(arr, target));
+            System.out.println("Try Again?");
+            boolean yn = scanner.nextBoolean();
+
+            if (yn) {
+                checking = checking;
+            } else if (!(yn)) {
+                checking = !checking;
+            }
+        }
     }
 
 }
--- src/com/company/RecursiveBinarySearch.java
+++ src/com/company/RecursiveBinarySearch.java
@@ -24,4 +24,22 @@ public class RecursiveBinarySearch {
             }
         }
     }
+
+    public static boolean myBinaryRecursionSearch(float[] arr, float target){
+        if(arr.length == 0){
+            return false;
+        }
+        else{
+            int midPoint = (int)Math.floor((double)arr.length/2);
+            if(arr[midPoint] == target){
+                return true;
+            }
+            else if(arr[midPoint] > target){
+                return myBinaryRecursionSearch(Arrays.copyOfRange(arr, midPoint + 1, arr.length),target);
+            }
+            else{
+                return myBinaryRecursionSearch(Arrays.copyOfRange(arr, 0, midPoint),target);
+            }
+        }
+    }
 }
