62b4da3dbb08822dcd4672944b50d16197df3e05
Yannis Lagrosa <378940@ga.aliefisd.net>
2022-07-24 04:07:29 -0500

    Updated Files

--- src/com/company/RecursiveBinarySearch.java
+++ src/com/company/RecursiveBinarySearch.java
@@ -42,4 +42,23 @@ public class RecursiveBinarySearch {
             }
         }
     }
+
+    boolean myRecursiveBinarySearch(float[] arr, float target){
+        if(arr.length == 0){
+            return false;
+        }
+        else{
+            int midPoint = (int)Math.floor((double)arr.length/2);
+            if(arr[midPoint] == target){
+                return true;
+            }
+            else if(arr[midPoint] > target){
+                return myRecursiveBinarySearch(Arrays.copyOfRange(arr,0, midPoint), target);
+            }
+            else{
+                return myRecursiveBinarySearch(Arrays.copyOfRange(arr,midPoint + 1, arr.length), target);
+            }
+        }
+    }
+
 }
