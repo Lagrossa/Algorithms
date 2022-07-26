0f8fe6c7ada950f69eba60ccc1612aadc05fd35c
Yannis Lagrosa <378940@ga.aliefisd.net>
2022-07-25 21:43:53 -0500

    Updated Files.

--- src/com/company/MergeSort.java
+++ src/com/company/MergeSort.java
@@ -1,7 +1,18 @@
 package com.company;
 
 public abstract class MergeSort {
-    public static void mergeSort(float[] list){
-        
+    /*
+    Sorts a given list in ascending order. The function will return a sorted list.
+     */
+    public static float[] mergeSort(float[] list){
+
+        /*
+        Divide: Divide the list into sorted sublists
+        Conquer: Recursively sort each list
+        Combine: Combine the sorted lists into a singular sorted list.
+         */
+        if(list.length <= 1){
+            return list;
+        }
     }
 }
